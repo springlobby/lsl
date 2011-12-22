3b693e58153074f689eda0660d5d91c0fec2ee00
Rene Milk <rene.milk@uni-muenster.de>
2011-12-22 18:18:39 +0100

    adds a minimal subset of boost::extension for dynamic libhandling

---
+++ test/usync.cpp
@@ -0,0 +1,45 @@
+//SpringUnitSync& usync();
+
+#include <iostream>
+#include <boost/extension/shared_library.hpp>
+#include <boost/function.hpp>
+
+void dummy()
+{
+    using namespace boost::extensions;
+
+      // In the Jamfile, shared libraries are set to have the same
+      // prefix and extension, even on different operating systems.
+      // This is for convenience in writing cross-platform code, but
+      // is not required. All shared libraries are set to start with
+      // "lib" and end with "extension".
+      std::string library_path = "libtutorial_1.extension";
+
+      // Create shared_library object with the relative or absolute
+      // path to the shared library.
+      shared_library lib(library_path);
+
+      // Attempt to open the shared library.
+      if (!lib.open()) {
+        std::cerr << "Library failed to open: " << library_path << std::endl;
+        return;
+      }
+      // Retrieve a function from the library, and store it in a Boost.Function
+       // object. It is also possible to use function pointers, but the syntax
+       // for Boost.Function is easier to understand. This retrieves a function
+       // called "boost_extension_hello_world" with a void return type and a single
+       // parameter of type int.
+       boost::function<void (int)>
+         f(lib.get<void, int>("boost_extension_hello_world"));
+
+       // Check that the function was found.
+       if (!f) {
+         std::cerr << "Function not found!" << std::endl;
+         return ;
+       }
+
+       // Call the function from the shared library with
+       // an integer parameter.
+       f(4);
+}
+
