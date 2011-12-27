628ebf697beee66d201081d6d279dc2a5da4bb59
Rene Milk <rene.milk@uni-muenster.de>
2011-12-27 20:25:44 +0100

    adds dummy logger

---
+++ src/utils/logging.h
@@ -0,0 +1,9 @@
+#ifndef LSL_LOGGING_H
+#define LSL_LOGGING_H
+
+#define LslError(msg) do {} while(0)
+#define LslDebug(msg) do {} while(0)
+#define LslWarning(msg) do {} while(0)
+#define LslDebug(msg) do {} while(0)
+
+#endif // LSL_LOGGING_H
