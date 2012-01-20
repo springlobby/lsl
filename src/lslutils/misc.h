a8ba17779bda6b88638dd4d98c3b19155e129cd3
Rene Milk <rene.milk@uni-muenster.de>
2012-01-20 17:37:01 +0100

    docs

--- src/lslutils/misc.h
+++ src/lslutils/misc.h
@@ -9,8 +9,13 @@
 
 #include "type_forwards.h"
 
+/** \file battle.h
+		\copyright GPL v2 **/
+
+//! mark function args to suppress warnings
 #define LSLUNUSED(identifier)
 
+//! mark local variables unused with this
 template <class T>
 inline void lslUnusedVar(const T& LSLUNUSED(t)) { }
 
@@ -20,11 +25,15 @@ class lslSize;
 template < class T > class lslColorBase;
 typedef lslColorBase<unsigned char> lslColor;
 
+//! the pseudo index returned for items not in given sequences
 static const int lslNotFound = -1;
 
 namespace Util {
 
+//! a collection of functors for use in std::find_if or LSL::Util::IndexInSequenceIf
 namespace Predicates {
+
+//! Functor to compare given string w/o considering case
 struct CaseInsensitive {
 	const std::string m_ref;
 	CaseInsensitive( const std::string r ) : m_ref(boost::to_lower_copy(r)){}
@@ -34,6 +43,13 @@ struct CaseInsensitive {
 };
 } //namespace Predicates {
 
+/** \brief convenience wrapper around boost::algorithm::split to split one string into a vector of strings
+ * \param msg the spring to be split
+ * \param seperators a list of seperaors, duh
+ * \param mode token_compress_off --> potentially empty strings in return,
+ 							token_compress_on --> empty tokens are discarded 
+ * \return all tokens in a vector, if msg contains no seperators, this'll contain msg as its only element
+ **/
 StringVector StringTokenize( const std::string& msg,
                              const std::string& seperators,
                              const boost::algorithm::token_compress_mode_type mode = boost::algorithm::token_compress_off )
@@ -49,18 +65,21 @@ bool FileExists( const std::string path );
 //! create temporary filestream, return is_open()
 bool FileCanOpen( const std::string path );
 
-template <typename T> T Clamp(const T var,const T min,const T max)
+//! return value in [min,max]
+template <typename T> 
+T Clamp(const T var,const T min,const T max)
 {
 	return ( (var < min) ? min : ( var > max ) ? max : var );
 }
 
+//! three argument version based on std::min
 template<typename T>
 T Min(T a, T b, T c)
 {
 	return std::min(a, std::min(b, c));
 }
 
-/** @brief Array with runtime determined size which is not initialized on creation.
+/** \brief Array with runtime determined size which is not initialized on creation.
 
 This RAII type is ment as output buffer for interfaces with e.g. C, where
 initializing a temp buffer to zero is waste of time because it gets overwritten
@@ -88,6 +107,11 @@ class uninitialized_array : public boost::noncopyable
 	T* elems;
 };
 
+/** \brief convenience wrapper around std::find
+ * \param ct any STL-like sequence
+ * \param val the value to search for
+ * \return index of val in ct, or lslNotFound
+ **/
 template < class StlContainer >
 inline int IndexInSequence( const StlContainer& ct,
 							const typename StlContainer::value_type& val )
@@ -99,6 +123,7 @@ inline int IndexInSequence( const StlContainer& ct,
 	return std::distance( ct.begin(), result );
 }
 
+//! IndexInSequence version for arbitrary comparison predicates
 template < class StlContainer, class Predicate >
 inline int IndexInSequenceIf( const StlContainer& ct,
 							const Predicate pred )
@@ -110,14 +135,18 @@ inline int IndexInSequenceIf( const StlContainer& ct,
 	return std::distance( ct.begin(), result );
 }
 
+/** \TODO docme **/
 lslColor GetFreeColor( const ConstUserPtr user );
 
+//! dll/module related Util functionality
 namespace Lib {
 enum Category {
 	Module,
 	Library
 };
+/** \TODO docme **/
 std::string GetDllExt();
+/** \TODO docme **/
 std::string CanonicalizeName(const std::string& name, Category cat);
 
 } // namespace Lib {
@@ -129,9 +158,12 @@ std::string AfterFirst( const std::string& phrase, const std::string& searchterm
 
 std::vector<lslColor>& GetBigFixColorsPalette( int numteams );
 bool AreColorsSimilar( const lslColor& col1, const lslColor& col2, int mindiff );
+//! tokenize input string and convert into rgb color
 lslColor ColorFromFloatString( const std::string& rgb_string );
 
+//! return some potentially quite inaccurate measure of cpu clock speed
 std::string GetHostCPUSpeed();
+//! for release/tarball verions, this is the tag, otherwise it's a compound of last tag, current git hash and commit distance
 std::string GetLibLobbyVersion();
 
 } //namespace Util {
@@ -154,6 +186,7 @@ public:
 	lslSize MakeFit(const lslSize bounds);
 };
 
+//! a color tuple class with arbitrarily typed fields
 template < typename T = unsigned char >
 class lslColorBase {
 private:
