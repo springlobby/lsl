41f24dca201ae8ba743568ad96dbdb412cde19ea
Rene Milk <rene.milk@uni-muenster.de>
2012-02-17 11:50:32 +0100

    added BSD license everywhere

--- src/lslutils/misc.h
+++ src/lslutils/misc.h
@@ -9,9 +9,6 @@
 
 #include "type_forwards.h"
 
-/** \file misc.h
-    \copyright GPL v2 **/
-
 //! mark function args to suppress warnings
 #define LSLUNUSED(identifier)
 
@@ -216,5 +213,32 @@ public:
 };
 
 } //namespace LSL {
+
+/**
+ * \file misc.h
+ * \section LICENSE
+Copyright 2012 by The libSpringLobby team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
 
 #endif // LSL_MISC_H
