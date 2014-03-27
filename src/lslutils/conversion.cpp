988c7ac22960d00d4905f6431092197d4d4fcbd3
abma <spring@abma.de>
2014-03-27 19:37:03 +0100

    safety

--- src/lslutils/conversion.cpp
+++ src/lslutils/conversion.cpp
@@ -12,7 +12,7 @@ std::wstring s2ws(const std::string& s)
 	const int len = MultiByteToWideChar(CP_ACP , 0, s.c_str(), slength, 0, 0);
 	wchar_t* buf = new wchar_t[len];
 	MultiByteToWideChar(CP_UTF8, 0, s.c_str(), slength, buf, len);
-	std::wstring r(buf);
+	std::wstring r(buf, len);
 	delete[] buf;
 	return r;
 }
