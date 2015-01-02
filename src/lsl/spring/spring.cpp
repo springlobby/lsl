ccf5db65a1850b7263b2d03fe44b1da7aa5db862
rlcevg <rlcevg@gmail.com>
2015-01-02 13:39:18 +0200

    Replace sett with config

--- src/lsl/spring/spring.cpp
+++ src/lsl/spring/spring.cpp
@@ -35,6 +35,8 @@
 #include <boost/filesystem/fstream.hpp>
 #include <boost/algorithm/string.hpp>
 
+#include <wx/filename.h>
+
 namespace LSL {
 
 namespace BF = boost::filesystem;
@@ -207,7 +209,8 @@ bool Spring::LaunchSpring( const std::string& params  )
     std::string cmd;// =  "\"" + sett().GetCurrentUsedSpringBinary();
 #ifdef __WXMAC__
     wxChar sep = wxFileName::GetPathSeparator();
-    if ( sett().GetCurrentUsedSpringBinary().AfterLast('.') == "app" )
+    std::string binary = Util::config().GetCurrentUsedSpringBinary();
+    if ( binary.substr(binary.rfind('.') + 1) == "app" )
         cmd += sep + std::string("Contents") + sep + std::string("MacOS") + sep + std::string("spring"); // append app bundle inner path
 #endif
     cmd += "\" " + configfileflags + params;
