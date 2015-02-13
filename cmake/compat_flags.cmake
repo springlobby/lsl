38a8f2bb38f9d5916443adfedae5cd59bdd99166
abma <spring@abma.de>
2015-02-13 23:11:34 +0100

    try to fix mingw compile

--- cmake/compat_flags.cmake
+++ cmake/compat_flags.cmake
@@ -1,9 +1,8 @@
-Include(TestCXXAcceptsFlag)
+include(TestCXXAcceptsFlag)
 execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
 				OUTPUT_VARIABLE GCC_VERSION)
 
 # try to use compiler flag -std=c++11
-include(TestCXXAcceptsFlag)
 MACRO(AddSTDFlag FLAG)
 	set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${FLAG} ")
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAG} ")
@@ -14,24 +13,52 @@ MACRO(AddSTDFlag FLAG)
 	set(CXX_STD0X_FLAGS "${FLAG}" )
 ENDMACRO(AddSTDFlag FLAG)
 
-if(WIN32)
-	CHECK_CXX_ACCEPTS_FLAG("-std=gnu++0x" CXX_FLAG_GXX0X)
-	if(CXX_FLAG_GXX0X)
-		AddSTDFlag("-std=gnu++0x")
-	endif(CXX_FLAG_GXX0X)
-else(WIN32)
-	CHECK_CXX_ACCEPTS_FLAG("-std=c++11" CXX_FLAG_CXX11)
-	if(CXX_FLAG_CXX11)
-		AddSTDFlag("-std=c++11")
-	else()
-		# try to use compiler flag -std=c++0x for older compilers
-		CHECK_CXX_ACCEPTS_FLAG("-std=c++0x" CXX_FLAG_CXX0X)
-		if(CXX_FLAG_CXX0X)
-			AddSTDFlag("-std=c++0x")
-		endif(CXX_FLAG_CXX0X)
-	endif(CXX_FLAG_CXX11)
-endif(WIN32)
+# try to use compiler flag -std=c++11
+
+set(CXX_STD0X_FLAGS FALSE)
+set(i 0)
+foreach(f -std=gnu++11 -std=c++11 -std=c++0x)
+	MATH(EXPR i "${i}+1") #cmake has working unset :-|
+	CHECK_CXX_ACCEPTS_FLAG("${f}" ACCEPTSFLAG${i})
+	if(${ACCEPTSFLAG${i}} AND NOT CXX_STD0X_FLAGS)
+		message(STATUS "Using ${f}")
+		AddSTDFlag("${f}")
+		set(CXX_STD0X_FLAGS TRUE)
+	endif()
+endforeach()
 
 if(NOT CXX_STD0X_FLAGS)
-	message(FATAL "you need a c++11 compatible compiler")
+	message(FATAL_ERROR "you need a c++11 compatible compiler")
 endif()
+
+If(NOT DEFINED LTO_FLAGS)
+	Set(LTO_FLAGS "")
+
+	Set(LTO       FALSE CACHE BOOL "Link Time Optimizations (LTO)")
+	If    (LTO)
+		CHECK_CXX_ACCEPTS_FLAG("-flto" HAS_LTO_FLAG)
+		If    (HAS_LTO_FLAG)
+			Set(LTO_FLAGS "${LTO_FLAGS} -flto")
+		Else  (HAS_LTO_FLAG)
+			Set(LTO_FLAGS "${LTO_FLAGS} -flto")
+		EndIf (HAS_LTO_FLAG)
+	EndIf (LTO)
+
+	Set(LTO_WHOPR FALSE CACHE BOOL "Link Time Optimizations (LTO) - Whole program optimizer (WHOPR)")
+	If    (LTO_WHOPR)
+		CHECK_CXX_ACCEPTS_FLAG("-fwhopr" HAS_LTO_WHOPR_FLAG)
+		If    (HAS_LTO_WHOPR_FLAG)
+			Set(LTO_FLAGS "${LTO_FLAGS} -fwhopr")
+		EndIf (HAS_LTO_WHOPR_FLAG)
+	EndIf (LTO_WHOPR)
+
+	If (LTO AND LTO_WHOPR)
+		Message(FATAL_ERROR "LTO and LTO_WHOPR are mutually exclusive, please enable only one at a time." )
+	EndIf (LTO AND LTO_WHOPR)
+
+EndIf (NOT DEFINED LTO_FLAGS)
+
+# Add Link-Time-Optimization flags, if supported (GCC >= 4.5) and enabled
+Set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS}           ${LTO_FLAGS}")
+Set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS}    ${LTO_FLAGS}")
+Set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${LTO_FLAGS}")
