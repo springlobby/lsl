7dfcaa0f7a0e7c4a759dafcfbcf6fc1353160cf2
abma <spring@abma.de>
2015-09-25 02:22:36 +0200

    fix lslextract / made useable

--- src/lslextract/lslextract.cpp
+++ src/lslextract/lslextract.cpp
@@ -1,4 +1,35 @@
-#include <lslunitsync/unitsync.h>
+#include "lslunitsync/unitsync.h"
+#include "lslunitsync/image.h"
+#include "lslutils/logging.h"
+#include "lslutils/type_forwards.h"
+#include "lslutils/config.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void lsllogerror(const char* format, ...)
+{
+	va_list args;
+	va_start (args, format);
+	vprintf (format, args);
+	va_end (args);
+	printf("\n");
+}
+void lsllogdebug(const char* format, ...)
+{
+	va_list args;
+	va_start (args, format);
+	vprintf (format, args);
+	va_end (args);
+	printf("\n");
+}
+void lsllogwarning(const char* format, ...)
+{
+	va_list args;
+	va_start (args, format);
+	vprintf (format, args);
+	va_end (args);
+	printf("\n");
+}
 
 
 void dump(LSL::StringVector& vec)
@@ -9,17 +40,41 @@ void dump(LSL::StringVector& vec)
 	}
 }
 
+void GetMapInfo(LSL::StringVector& maps)
+{
+	for(const std::string mapname: maps) {
+		lsllogdebug("Extracting %s", mapname.c_str());
+		LSL::usync().GetMetalmap(mapname, 512, 512);
+		LSL::usync().GetHeightmap(mapname, 512, 512);
+		LSL::usync().GetMinimap(mapname, 512, 512);
+		LSL::usync().GetMap(mapname);
+	}
+}
+
+void GetGameInfo()
+{
+}
+
+void GetAIInfo()
+{
+}
+
 int main(int argc, char* argv[])
 {
-	if (argc != 2) {
-		printf("Usage: %s <unitsync path>\n", argv[0]);
+	if (argc != 3) {
+		printf("Usage: %s <cache dir> <unitsync path>\n", argv[0]);
 		return 1;
 	}
-	LSL::usync().LoadUnitSyncLib(argv[1]);
-	LSL::StringVector games = LSL::usync().GetModList();
-	dump(games);
+	LSL::Util::config().ConfigurePaths(argv[1], argv[2], "");
+	LSL::usync().LoadUnitSyncLib(argv[2]);
+
+	//LSL::StringVector games = LSL::usync().GetModList();
+	//dump(games);
 	LSL::StringVector maps = LSL::usync().GetMapList();
-	dump(maps);
+
+	//dump(maps);
+	GetMapInfo(maps);
+
 	//LSL::usync().
 	LSL::usync().FreeUnitSyncLib();
 }
