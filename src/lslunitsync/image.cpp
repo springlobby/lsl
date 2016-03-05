8b5bfb3e99e7e06ae499c7f35cfac0a6a92dfd9b
abma <spring@abma.de>
2016-03-05 17:46:30 +0100

    fix #ifdef logic: won't break when fmemopen appears on windows

--- src/lslunitsync/image.cpp
+++ src/lslunitsync/image.cpp
@@ -21,6 +21,7 @@
 #include <stdio.h> //fmemopen
 
 
+#if !defined(HAVE_FMEMOPEN)
 #if defined(WIN32)
 //! we need our own fmemopen implementation since its posix only
 FILE* fmemopen(void* data, size_t size, const char* mode)
@@ -41,9 +42,10 @@ FILE* fmemopen(void* data, size_t size, const char* mode)
 	std::string modeString(mode);
 	return _wfopen(tempFile.c_str(), std::wstring(modeString.begin(), modeString.end()).c_str());
 }
-#elif !defined(HAVE_FMEMOPEN)
+#else //WIN32
 #error no workarround for missing fmemopen!
 #endif
+#endif // !defined(HAVE_FMEMOPEN)
 
 namespace cimg_library
 {
