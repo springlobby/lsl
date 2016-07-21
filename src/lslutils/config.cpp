9127683fd58fa7b03cb20f8498c85bd6acc6ff8d
abma <spring@abma.de>
2016-07-21 08:18:03 +0200

    try to fix compile

--- src/lslutils/config.cpp
+++ src/lslutils/config.cpp
@@ -8,15 +8,12 @@
 #include "globalsmanager.h"
 
 #ifdef WIN32
+#ifdef _POSIX_
+#undef _POSIX_
+#endif
 #include <stdlib.h>
 int setenv(const char *name, const char *value, int overwrite)
 {
-    int errcode = 0;
-    if(!overwrite) {
-        size_t envsize = 0;
-        errcode = getenv_s(&envsize, NULL, 0, name);
-        if(errcode || envsize) return errcode;
-    }
     return _putenv_s(name, value);
 }
 #endif
